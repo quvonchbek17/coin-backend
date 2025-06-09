775f88581ee331656a458d96ff000351c4ff4665
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2025-06-09 22:14:33 +0500

    port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
 
-  const PORT = parseInt(process.env.SERVER_PORT) || 5000;
+  const PORT = parseInt(process.env.SERVER_PORT) || 3377;
   const logger = new Logger('Main');
 
 
