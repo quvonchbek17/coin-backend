f1781eaf51d213236d8567ec70ea393def279e80
quvonchbek17 <94097313+quvonchbek17@users.noreply.github.com>
2025-06-11 12:01:27 +0500

    Update users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersGateway } from './users.gateway';
 import { MongoModule } from 'modules/mongo';
+import { CoinsService } from 'modules/coins/coins.service';
 
 @Module({
   imports: [MongoModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersGateway]
+  providers: [UsersService, UsersGateway, CoinsService]
 })
 export class UsersModule {}
