529cea90cd267e871c41a3297f8b89277d1ed963
quvonchbek17 <94097313+quvonchbek17@users.noreply.github.com>
2025-06-11 12:00:36 +0500

    Update coins.service.ts

--- src/modules/coins/coins.service.ts
+++ src/modules/coins/coins.service.ts
@@ -10,21 +10,19 @@ import { Cache } from 'cache-manager';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Users, usersDocument } from 'modules/mongo';
-import { UsersService } from 'modules/users/users.service';
 
 @Injectable()
 export class CoinsService implements OnModuleInit, OnModuleDestroy {
   private readonly BATCH_INTERVAL = 5000;
   private BATCH_COUNT: number = 1;
   private readonly CLEAR_INTERVAL = 1000 * 60 * 2;
-  private pendingUpdates: { [userId: string]: Users } = {};
+  private pendingUpdates: { [id: string]: Users } = {};
   private intervalId: NodeJS.Timeout;
   private readonly logger = new Logger(CoinsService.name);
 
   constructor(
     @InjectModel(Users.name) private userModel: Model<usersDocument>,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
-    private readonly usersService: UsersService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
   ) { }
 
   onModuleInit() {
@@ -42,105 +40,90 @@ export class CoinsService implements OnModuleInit, OnModuleDestroy {
     try {
       let redisUsers: Users[] = (await this.cacheManager.get<Users[]>('users')) || [];
 
-      const existingUserIndex = redisUsers.findIndex(user => user.id === body.userId);
+      const existingUserIndex = redisUsers.findIndex(user => user.id === body.id);
       if (existingUserIndex !== -1) {
-        redisUsers[existingUserIndex].coins += body.coinCount;
-        redisUsers[existingUserIndex].energy = body.energy;
-        redisUsers[existingUserIndex].energyCapacity = body.energyCapacity;
-        redisUsers[existingUserIndex].level = body.level;
-        redisUsers[existingUserIndex].totalTaps = body.totalTaps;
-        redisUsers[existingUserIndex].energyQuality = body.energyQuality;
-        redisUsers[existingUserIndex].clickQuality = body.clickQuality;
+        let redisUser = redisUsers[existingUserIndex]
+        redisUsers[existingUserIndex].coins = body.coinCount ? redisUser.coins + body.coinCount : redisUser.coins;
+        redisUsers[existingUserIndex].energy = body.energy ? body.energy : redisUser.energy;
+        redisUsers[existingUserIndex].energyCapacity = body.energyCapacity ? body.energyCapacity : redisUser.energyCapacity;
+        redisUsers[existingUserIndex].level = body.level ? body.level : redisUser.level;
+        redisUsers[existingUserIndex].totalTaps = body.totalTaps ? body.totalTaps : redisUser.totalTaps;
+        redisUsers[existingUserIndex].energyQuality = body.energyQuality ? body.energyQuality : redisUser.energyQuality;
+        redisUsers[existingUserIndex].clickQuality = body.clickQuality ? body.clickQuality : redisUser.clickQuality;
         redisUsers[existingUserIndex].lastClickDate = new Date();
+
+        let seconds = this.secondsPassedSince(redisUser.lastCalculatedEnergyDate)
+        let energy = seconds > 0 ? redisUser.energy + seconds * redisUser.energyQuality : redisUser.energy
+        redisUsers[existingUserIndex].energy = energy >= redisUser.energyCapacity ? redisUser.energyCapacity : energy
         redisUsers[existingUserIndex].lastCalculatedEnergyDate = new Date();
+
         await this.cacheManager.set('users', redisUsers);
-        this.pendingUpdates[body.userId] = redisUsers[existingUserIndex];
+        this.pendingUpdates[body.id] = redisUsers[existingUserIndex];
         return redisUsers[existingUserIndex];
       } else {
 
-        let user = await this.userModel.findOne({ id: body.userId }).exec();
-        if (!user) {
-          user = new this.userModel({ id: body.userId, coins: 0 });
-        }
-        user.coins += body.coinCount;
+        let user = await this.userModel.findOne({ id: body.id }).exec();
+        user.coins = body.coinCount ? body.coinCount + user.coins : user.coins;
+
+        let seconds = this.secondsPassedSince(user.lastCalculatedEnergyDate)
+        let energy = seconds > 0 ? user.energy + seconds * user.energyQuality : user.energy
+        redisUsers[existingUserIndex].energy = energy >= user.energyCapacity ? user.energyCapacity : energy
+        redisUsers[existingUserIndex].lastCalculatedEnergyDate = new Date();
+
         redisUsers.push(user.toObject());
         await this.cacheManager.set('users', redisUsers);
-        this.pendingUpdates[body.userId] = user.toObject()
+        this.pendingUpdates[body.id] = user.toObject()
         return user.toObject();
       }
     } catch (error) {
-      this.logger.error(`Error updating coins for user ${body.userId}: ${error.message}`);
+      this.logger.error(`Error updating coins for user ${body.id}: ${error.message}`);
       throw new Error(`Failed to update coins: ${error.message}`);
     }
   }
 
-    async getUserDatas(body: any): Promise<Partial<Users>> {
+  async getUserDatas(body: any): Promise<Partial<Users>> {
     try {
       let redisUsers: Users[] = (await this.cacheManager.get<Users[]>('users')) || [];
       const existingUserIndex = redisUsers.findIndex(user => user.id === body.id);
       if (existingUserIndex !== -1) {
-      let user = redisUsers[existingUserIndex]
-      let seconds = this.usersService.secondsPassedSince(user.lastCalculatedEnergyDate)
-      let energy = seconds > 0 ? user.energy + seconds * user.energyQuality: user.energy
-      redisUsers[existingUserIndex].energy = energy >= user.energyCapacity ? user.energyCapacity: energy
-      redisUsers[existingUserIndex].lastCalculatedEnergyDate = new Date()
-      await this.cacheManager.set('users', redisUsers);
-      return redisUsers[existingUserIndex];
+        let user = redisUsers[existingUserIndex]
+        let seconds = this.secondsPassedSince(user.lastCalculatedEnergyDate)
+        let energy = seconds > 0 ? user.energy + seconds * user.energyQuality : user.energy
+        redisUsers[existingUserIndex].energy = energy >= user.energyCapacity ? user.energyCapacity : energy
+        redisUsers[existingUserIndex].lastCalculatedEnergyDate = new Date()
+        await this.cacheManager.set('users', redisUsers);
+        return redisUsers[existingUserIndex];
       } else {
         let user = await this.userModel.findOne({ id: body.id });
+        
+        let seconds = this.secondsPassedSince(user.lastCalculatedEnergyDate)
+        let energy = seconds > 0 ? user.energy + seconds * user.energyQuality : user.energy
+        redisUsers[existingUserIndex].energy = energy >= user.energyCapacity ? user.energyCapacity : energy
+        redisUsers[existingUserIndex].lastCalculatedEnergyDate = new Date()
+
         redisUsers.push(user.toObject());
         await this.cacheManager.set('users', redisUsers);
-        this.pendingUpdates[body.userId] = user.toObject()
+        this.pendingUpdates[body.id] = user.toObject()
         return user.toObject();
       }
     } catch (error) {
-      this.logger.error(`Error updating coins for user ${body.userId}: ${error.message}`);
+      this.logger.error(`Error updating coins for user ${body.id}: ${error.message}`);
       throw new Error(`Failed to update coins: ${error.message}`);
     }
   }
 
-  // async updateEnergy(body: any): Promise<Partial<Users>> {
-  //   try {
-  //     let redisUsers: Users[] = (await this.cacheManager.get<Users[]>('users')) || [];
-
-  //     const existingUserIndex = redisUsers.findIndex(user => user.id === body.userId);
-  //     if (existingUserIndex !== -1) {
-  //       redisUsers[existingUserIndex].energy = redisUsers[existingUserIndex].energy >= redisUsers[existingUserIndex].energyCapacity? redisUsers[existingUserIndex].energyCapacity: body.energy;
-  //       redisUsers[existingUserIndex].lastCalculatedEnergyDate = new Date();
-  //       await this.cacheManager.set('users', redisUsers);
-  //       this.pendingUpdates[body.userId] = redisUsers[existingUserIndex];
-  //       return redisUsers[existingUserIndex];
-  //     } else {
-  //       let user = await this.userModel.findOne({ id: body.userId }).exec();
-  //       if (!user) {
-  //         user = new this.userModel({ id: body.userId, coins: 0 });
-  //       }
-  //       user.energy = body.energy;
-  //       redisUsers.push(user.toObject());
-  //       await this.cacheManager.set('users', redisUsers);
-  //       this.pendingUpdates[body.userId] = user.toObject()
-  //       return user.toObject();
-  //     }
-  //   } catch (error) {
-  //     this.logger.error(`Error updating coins for user ${body.userId}: ${error.message}`);
-  //     throw new Error(`Failed to update coins: ${error.message}`);
-  //   }
-  // }
-
   async flushPendingUpdates(count: number) {
     try {
-      this.BATCH_COUNT = count  + 1
-      if(this.BATCH_COUNT > 20){
+      this.BATCH_COUNT = count + 1
+      if (this.BATCH_COUNT > 20) {
         await this.cacheManager.set('users', []);
         this.BATCH_COUNT = 1
       }
       if (Object.keys(this.pendingUpdates).length === 0) return;
 
-
-
-      const bulkOps = Object.entries(this.pendingUpdates).map(([userId, user]) => ({
+      const bulkOps = Object.entries(this.pendingUpdates).map(([id, user]) => ({
         updateOne: {
-          filter: { id: userId },
+          filter: { id: id },
           update: {
             coins: user.coins,
             level: user.level,
@@ -160,12 +143,12 @@ export class CoinsService implements OnModuleInit, OnModuleDestroy {
 
       let redisUsers: Users[] = (await this.cacheManager.get<Users[]>('users')) || [];
 
-      for (const [userId, user] of Object.entries(this.pendingUpdates)) {
-        const userIndex = redisUsers.findIndex(user => user.id === userId);
+      for (const [id, user] of Object.entries(this.pendingUpdates)) {
+        const userIndex = redisUsers.findIndex(user => user.id === id);
         if (userIndex !== -1) {
           redisUsers[userIndex] = user;
         } else {
-          const dbUser = await this.userModel.findOne({ id: userId }).exec();
+          const dbUser = await this.userModel.findOne({ id: id }).exec();
           if (dbUser) {
             redisUsers.push(dbUser.toObject());
           }
@@ -179,26 +162,11 @@ export class CoinsService implements OnModuleInit, OnModuleDestroy {
     }
   }
 
-  async getUserCoins(userId: string): Promise<{ userId: string; totalCoins: number }> {
-    try {
-      let redisUsers: Users[] = (await this.cacheManager.get<Users[]>('users')) || [];
-      const cachedUser = redisUsers.find(user => user.id === userId);
-      if (cachedUser) {
-        return { userId, totalCoins: cachedUser.coins };
-      }
-
-      const user = await this.userModel.findOne({ id: userId }).exec();
-      const totalCoins = user ? user.coins : 0;
-
-      if (user) {
-        redisUsers.push(user.toObject());
-        await this.cacheManager.set('users', redisUsers);
-      }
-
-      return { userId, totalCoins };
-    } catch (error) {
-      this.logger.error(`Error getting coins for user ${userId}: ${error.message}`);
-      throw new Error(`Failed to get coins: ${error.message}`);
-    }
+    secondsPassedSince(givenDate: Date): number {
+    const now = new Date(); // Hozirgi vaqt
+    const timeDifference = now.getTime() - givenDate.getTime(); // Millisekundlarda farq
+    const seconds = Math.floor(timeDifference / 1000); // Sekundlarga aylantirish
+    return seconds;
   }
+
 }
