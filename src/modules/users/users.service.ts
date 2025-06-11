1740145faeabab287a2ec4ecb8413d002d5cb7fa
quvonchbek17 <94097313+quvonchbek17@users.noreply.github.com>
2025-06-11 12:02:11 +0500

    Update users.service.ts

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,13 +5,15 @@ import { Users, usersDocument } from 'modules/mongo';
 import { Model } from 'mongoose';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { CoinsService } from 'modules/coins/coins.service';
 
 @Injectable()
 export class UsersService implements OnModuleInit, OnModuleDestroy {
   private readonly BATCH_INTERVAL = 5000;
-  private pendingUpdates: { [userId: string]: number } = {};
+  private pendingUpdates: { [id: string]: number } = {};
   private intervalId: NodeJS.Timeout;
   private readonly logger = new Logger(UsersService.name);
+
   onModuleInit() {
     this.logger.log('CoinsService initialized with in-memory cache.');
   }
@@ -23,14 +25,12 @@ export class UsersService implements OnModuleInit, OnModuleDestroy {
   constructor(
     @InjectModel(Users.name) private userModel: Model<usersDocument>,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly coinService: CoinsService
   ) { }
   async createOrGetUser(body: CreateUserDto): Promise<any> {
     let user = await this.userModel.findOne({ id: body.id })
     if (user) {
-      let seconds = this.secondsPassedSince(user.lastCalculatedEnergyDate)
-      let energy = seconds > 0 ? user.energy + seconds * user.energyQuality : user.energy
-      user.energy = energy >= user.energyCapacity ? user.energyCapacity : energy
-      user.lastCalculatedEnergyDate = new Date()
+      let userData = await this.coinService.getUserDatas({id: user.id})
       if (!user.refCode) {
         let code = this.generateCode();
         let existingCode = await this.userModel.findOne({ refCode: code });
@@ -41,7 +41,8 @@ export class UsersService implements OnModuleInit, OnModuleDestroy {
         user.refCode = code
         user.save()
       }
-      return user
+      userData.refCode = user.refCode
+      return userData
     } else {
       let code = this.generateCode();
       let existingCode = await this.userModel.findOne({ refCode: code });
@@ -54,21 +55,6 @@ export class UsersService implements OnModuleInit, OnModuleDestroy {
     }
   }
 
-  async findById(userId: string): Promise<any> {
-    let user = await this.userModel.findOne({ id: userId })
-    if (user && user.lastCalculatedEnergyDate) {
-      let seconds = this.secondsPassedSince(user.lastCalculatedEnergyDate)
-      let energy = seconds > 0 ? user.energy + seconds * user.energyQuality : user.energy
-      user.energy = energy >= user.energyCapacity ? user.energyCapacity : energy
-      user.lastCalculatedEnergyDate = new Date()
-      user.save()
-      return user
-    }
-
-    if (user) {
-      return user
-    }
-  }
 
   generateCode(length = 15) {
     const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -80,10 +66,4 @@ export class UsersService implements OnModuleInit, OnModuleDestroy {
     return code;
   }
 
-  secondsPassedSince(givenDate: Date): number {
-    const now = new Date(); // Hozirgi vaqt
-    const timeDifference = now.getTime() - givenDate.getTime(); // Millisekundlarda farq
-    const seconds = Math.floor(timeDifference / 1000); // Sekundlarga aylantirish
-    return seconds;
-  }
 }
